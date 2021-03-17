4f3471d831b1f9cfcc992a7090dd9123ce4d3cd9
Félix Desjardins <felix@felixinx.me>
2021-03-17 16:43:02 -0400

    :coffin: Remove light styles

--- vuetify.options.js
+++ vuetify.options.js
@@ -4,18 +4,14 @@ export default function ({ app }) {
     theme: {
       themes: {
         dark: {
-          'primary-light': '#4574aa',
           primary: '#00497b',
           'primary-dark': '#00224e',
-          'secondary-light': '#53cbbd',
           secondary: '#009a8d',
           'secondary-dark': '#006b60',
         },
         light: {
-          'primary-light': '#5fa2dd',
           primary: '#2374ab',
           'primary-dark': '#00497b',
-          'secondary-light': '#85ffef',
           secondary: '#4dccbd',
           'secondary-dark': '#009a8d',
         },
