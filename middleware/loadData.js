011ceac02e4f3b9d70f4c62bbe5b87ae1305a022
Félix Desjardins <felixinx@gmail.com>
2021-04-27 13:34:59 -0400

    ♻️ Refactor middleware

--- middleware/loadData.js
+++ middleware/loadData.js
@@ -1,24 +1,30 @@
-export default async ({ error, store, route }) => {
-  if (store.state.app.dataIsLoaded) {
-    if (route.params.region)
-      store.commit('settings/set', {
-        setting: 'currentRegion',
-        value: route.params.region,
-      })
+export default async ({ error, store, params }) => {
+  // User is visiting a route where data is not neccessary
+  if (!params.region) return
 
-    return
+  // User has never loaded app
+  if (!store.state.app.dataIsLoaded) {
+    await store.dispatch('app/loadData', params.region)
   }
 
-  if (route.name.includes('regions')) {
-    const shouldRedirect = await store.dispatch(
-      'app/loadData',
-      route.params.region
-    )
-    if (shouldRedirect) return
-
+  // Check if region is valid
+  if (!(params.region in store.state.regions.data)) {
     return error({
       message: 'Wrong region / Mauvaise région',
       statusCode: 404,
     })
   }
+
+  // User has loaded app and continues with the same region
+  if (store.state.settings.currentRegion === params.region) return
+
+  // User has loaded app but is changing region
+  store.dispatch(
+    'settings/changeRegion',
+    store.state.regions.data[params.region]
+  )
+  store.commit('settings/set', {
+    setting: 'currentRegion',
+    value: params.region,
+  })
 }
