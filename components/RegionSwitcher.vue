e807681bc123f4bea3035afcd3cfb984c0897fbe
Félix Desjardins <felixinx@gmail.com>
2021-04-29 13:06:29 -0400

    🎨 Format code

--- components/RegionSwitcher.vue
+++ components/RegionSwitcher.vue
@@ -44,7 +44,9 @@
           <v-card tile>
             <v-img
               height="250"
-              :src="`${host}/storage/content/regions/${region.image || 'default.jpg'}`"
+              :src="`${host}/storage/content/regions/${
+                region.image || 'default.jpg'
+              }`"
               gradient="to bottom, transparent 0%, rgba(0,73,123,1) 100%"
             ></v-img>
             <v-card-title>{{ region.name }}</v-card-title>
