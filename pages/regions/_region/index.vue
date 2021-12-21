7a6ee39c8f484deca15f59d15e35d3e34c092f77
Félix Desjardins <felixinx@gmail.com>
2021-12-21 18:11:42 -0500

    💄 Snow

--- pages/regions/_region/index.vue
+++ pages/regions/_region/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-14">
-    <div class="secondary tt-texture--icons">
+    <div class="secondary tt-brand tt-texture--icons">
       <v-container class="d-flex align-center pa-6">
         <div class="flex-grow-1">
           <h1 class="text-h5 font-weight-medium">
@@ -32,6 +32,9 @@
           class="welcome-logo ml-4"
         />
       </v-container>
+      <div class="tt-winter overflow-hidden">
+        <div v-for="i in 50" :key="i" class="tt-winter__snowflake"></div>
+      </div>
     </div>
     <v-container>
       <v-row>
@@ -283,6 +286,50 @@ export default {
   &__text {
     height: 100%;
     max-width: 90%;
+  }
+}
+
+.tt-brand {
+  position: relative;
+}
+
+.tt-winter {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  &__snowflake {
+    --size: 1vw;
+    width: var(--size);
+    height: var(--size);
+    background: white;
+    border-radius: 50%;
+    position: absolute;
+    top: -5vh;
+  }
+}
+
+@keyframes snowfall {
+  0% {
+    transform: translate3d(var(--left-ini), 0, 0);
+  }
+  100% {
+    transform: translate3d(var(--left-end), 110vh, 0);
+  }
+}
+
+@for $i from 1 through 50 {
+  .tt-winter__snowflake:nth-child(#{$i}) {
+    --size: #{random(10) + 4}px;
+    --left-ini: #{random(20) - 10}vw;
+    --left-end: #{random(20) - 10}vw;
+    left: #{random(100)}vw;
+    animation: snowfall #{5 + random(10)}s linear infinite;
+    animation-delay: -#{random(10)}s;
   }
 }
 </style>
