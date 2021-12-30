065b72e4fb6f7792c5f43d644ebc5c8d8eef62e8
Félix Desjardins <felixinx@gmail.com>
2021-12-29 19:12:09 -0500

    💄 Removed short name in the agency settings panel

--- components/settings/SettingsAgencies.vue
+++ components/settings/SettingsAgencies.vue
@@ -81,9 +81,6 @@
               </v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action v-if="!agency.regions.includes('*')">
-              <v-list-item-action-text>
-                {{ agency.shortName }}
-              </v-list-item-action-text>
               <v-btn
                 icon
                 :small="$vuetify.breakpoint.smAndDown"
