9ebbe418a8d4a9fbc41705d7b305604212b05e1a
Félix Desjardins <felixinx@gmail.com>
2022-02-08 17:30:47 -0500

    🐛 Increase skeleton width to fit actual links

--- components/map/MapBottomSheet.vue
+++ components/map/MapBottomSheet.vue
@@ -11,7 +11,7 @@
         <v-skeleton-loader
           v-if="link.loading || !link.url"
           class="mr-4 my-2"
-          width="200px"
+          width="275px"
           min-height="62px"
           max-height="62px"
           type="image"
