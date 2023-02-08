0d78b346a93ab32316ad15f699d3f362eabadcbe
Félix Desjardins <FelixINX@users.noreply.github.com>
2023-02-08 14:21:42 -0500

    🐛 Support credentials in Axios

--- plugins/axios.client.js
+++ plugins/axios.client.js
@@ -1,4 +1,6 @@
 export default function ({ $axios, app, error, redirect, store }) {
+  $axios.defaults.withCredentials = true
+
   $axios.onRequest((config) => {
     config.headers = {
       'Content-Language': store.state.settings.lang || app.i18n.locale,
