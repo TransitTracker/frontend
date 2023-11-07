56f643d488502c42078820203e938a9d94d7cef9
Félix Desjardins <FelixINX@users.noreply.github.com>
2023-11-07 15:55:36 -0500

    :bookmark: 2.9.7 (5 years update)

--- pages/regions/_region/index.vue
+++ pages/regions/_region/index.vue
@@ -44,6 +44,71 @@
           </TwFilledButton>
         </div>
       </div>
+      <div class="tw-absolute tw-inset-0 tw-h-full tw-w-full">
+        <div
+          class="tw-container tw-mx-auto tw-mt-20 tw-flex tw-flex tw-w-full tw-justify-end tw-px-4"
+        >
+          <div
+            title="Confetti"
+            class="tw-flex tw-cursor-pointer tw-items-center tw-gap-4 tw-rounded-xl tw-bg-gradient-to-br tw-from-primary-40/90 tw-to-secondary-40/90 tw-px-4 tw-py-2 tw-text-white dark:tw-from-primary-80/90 dark:tw-to-secondary-80/90 dark:tw-text-neutral-10"
+            @click="launchConfetti"
+          >
+            <svg
+              class="tw-h-16 tw-shrink-0"
+              viewBox="0 0 155 200"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M76.85 0c-5.47 8.864-6.838 12.273-2.735 15 1.367.682 2.05 1.023 2.735 1.023.683 0 1.367-.341 2.735-1.023 4.103-2.727 2.735-6.136-2.735-15Zm-8.206 40.52V26.59c-1.368 0-2.736-1.363-2.736-2.726 0-1.364 1.368-2.728 2.736-2.728h16.411c1.368 0 2.736 1.364 2.736 2.728 0 1.363-1.368 2.727-2.736 2.727V40.65a57.168 57.168 0 0 1 8.845 2.068l5.618-12.011c-1.24-.577-1.901-2.389-1.323-3.625.578-1.236 2.395-1.895 3.635-1.319l14.874 6.916c1.239.576 1.901 2.388 1.323 3.624-.578 1.236-2.396 1.896-3.635 1.32l-5.739 12.27a58.349 58.349 0 0 1 9.321 7.851l11.321-9.472c-.879-1.044-.71-2.965.338-3.842 1.047-.876 2.974-.708 3.853.336l10.549 12.536c.879 1.044.711 2.965-.337 3.842-1.047.876-2.974.708-3.853-.336l-12.252 10.25c4.275 8.12 6.698 17.377 6.698 27.194 0 33.883-34.931 79.515-50.564 98.253a9.543 9.543 0 0 1-14.764 0c-15.535-18.738-50.466-64.37-50.466-98.253 0-10.334 2.684-20.046 7.389-28.467l-10.731-8.977c-.88 1.044-2.806 1.212-3.854.336-1.047-.877-1.216-2.798-.337-3.843l10.55-12.535c.879-1.044 2.805-1.212 3.853-.336 1.048.877 1.216 2.798.337 3.842l10.11 8.458a58.367 58.367 0 0 1 7.806-6.485l-5.903-12.623c-1.24.577-3.057-.083-3.635-1.319-.578-1.236.083-3.048 1.323-3.624l14.874-6.916c1.24-.576 3.057.083 3.635 1.32.578 1.235-.084 3.047-1.323 3.624l5.707 12.203a57.166 57.166 0 0 1 10.346-2.39Zm49.582-30.455c-8.715 5.728-11.4 8.241-8.837 12.442.95 1.193 1.425 1.79 2.044 2.079.62.289 1.384.268 2.914.226 4.874-.743 5.08-4.409 3.879-14.747ZM37.762 24.812c-4.874-.743-5.08-4.409-3.879-14.747 8.715 5.728 11.4 8.241 8.837 12.442-.95 1.193-1.425 1.79-2.044 2.079-.62.289-1.384.268-2.914.226ZM155 37.448c-10.326 1.52-13.825 2.666-13.283 7.553.355 1.48.533 2.222.971 2.744.44.524 1.141.827 2.546 1.434 4.732 1.38 6.472-1.855 9.766-11.731ZM9.766 49.178C5.033 50.56 3.294 47.324 0 37.448 10.326 38.966 13.825 40.112 13.283 45c-.711 2.966-.711 2.966-3.517 4.178Zm88.198 14.023H57.453l-4.869 38.425 16.165 3.502c.52-.973 1.332-1.849 2.435-2.627 1.104-.779 2.467-1.167 4.09-1.167 2.467 0 4.415.778 5.843 2.335 1.429 1.556 2.143 3.372 2.143 5.447 0 2.4-.747 4.313-2.24 5.739-1.428 1.362-3.31 2.043-5.648 2.043-1.558 0-3.02-.324-4.383-.973-1.364-.648-2.305-1.653-2.824-3.015l-17.432 3.502c.91 3.373 2.728 6.258 5.455 8.658 2.791 2.399 6.005 4.248 9.64 5.545 3.636 1.232 7.239 1.848 10.81 1.848 4.998 0 9.38-1.006 13.146-3.016 3.83-2.075 6.816-4.961 8.959-8.657 2.207-3.697 3.312-7.945 3.312-12.744 0-3.891-.877-7.296-2.63-10.215-1.688-2.983-3.96-5.318-6.817-7.004-2.857-1.75-5.94-2.658-9.251-2.723-3.506-.13-6.59.325-9.252 1.362-1.247.456-2.4.982-3.476 1.566l1.957-11.488h25.378V63.2Z"
+                fill="#fff"
+              />
+              <mask
+                id="a"
+                style="mask-type: alpha"
+                maskUnits="userSpaceOnUse"
+                x="18"
+                y="40"
+                width="117"
+                height="160"
+              >
+                <path
+                  d="M76.394 40c-31.94 0-57.897 26.116-57.897 58.252 0 33.883 34.93 79.515 50.466 98.253a9.543 9.543 0 0 0 14.764 0c15.632-18.738 50.564-64.37 50.564-98.253C134.291 66.116 108.334 40 76.394 40ZM57.452 63.201h40.512v16.343H72.586l-1.958 11.488a25.931 25.931 0 0 1 3.477-1.566c2.662-1.037 5.746-1.491 9.251-1.362 3.311.065 6.395.972 9.252 2.723 2.856 1.686 5.128 4.021 6.816 7.005 1.753 2.918 2.63 6.323 2.63 10.214 0 4.799-1.104 9.047-3.311 12.744-2.143 3.696-5.13 6.582-8.96 8.657-3.765 2.01-8.147 3.016-13.146 3.016-3.57 0-7.174-.616-10.81-1.848-3.635-1.297-6.848-3.146-9.64-5.545-2.727-2.4-4.545-5.285-5.454-8.658l17.431-3.502c.52 1.362 1.461 2.367 2.825 3.015 1.363.649 2.824.973 4.383.973 2.337 0 4.22-.681 5.648-2.043 1.493-1.426 2.24-3.339 2.24-5.739 0-2.075-.715-3.891-2.143-5.447-1.428-1.557-3.377-2.335-5.844-2.335-1.623 0-2.986.388-4.09 1.167-1.103.778-1.915 1.654-2.434 2.627l-16.165-3.502 4.868-38.425Z"
+                  fill="#fff"
+                />
+              </mask>
+              <g mask="url(#a)">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M25.943 143.754c1.405-2.786 2.811-5.572 5.622-5.572 2.948 0 4.623 1.244 6.217 4.159 1.446 2.644 2.826 4.258 5.029 4.258 2.275 0 3.831-1.782 5.415-4.519 1.639-2.833 3.306-3.898 5.83-3.898 2.569 0 4.34 1.17 6.047 4.052 1.59 2.683 3.125 4.365 5.198 4.365 1.975 0 3.252-1.443 4.633-3.986 1.625-2.993 3.394-4.431 6.612-4.431 3.064 0 4.551 1.134 5.994 4 1.28 2.542 2.527 4.106 4.809 4.39 2.502-.293 3.828-2.01 5.153-4.636 1.405-2.786 2.811-3.754 5.622-3.754 2.948 0 4.623 1.244 6.217 4.159 1.446 2.644 2.825 4.258 5.028 4.258 2.276 0 3.832-1.782 5.415-4.519 1.639-2.833 3.307-3.898 5.83-3.898 2.57 0 4.341 1.17 6.048 4.052 1.59 2.683 3.125 7.092 5.198 7.092 1.975 0 3.252-2.352 4.633-4.895 1.625-2.992 3.393-6.249 6.612-6.249 3.064 0 4.551 2.952 5.994 5.818 1.361 2.702 2.683 5.326 5.251 5.326l-34.728 37.038-36.39-10.455-30.169 10.455-32.743-37.038c2.812 0 4.217-2.786 5.623-5.572Z"
+                  fill="#96CCFF"
+                />
+                <path
+                  d="M45.85 150c-7.75 0-7.75 15.455-15.5 15.455l45.132 51.363 47.868-51.363c-7.295 0-7.295-15.455-15.5-15.455-8.206 0-9.574 15.455-15.5 15.455-5.927 0-8.662-15.455-15.5-15.455-6.839 0-9.118 15.455-15.5 15.455-6.383 0-7.75-15.455-15.5-15.455Z"
+                  fill="#001D32"
+                />
+              </g>
+            </svg>
+            <div class="tw-max-w-xl tw-text-left">
+              <h1
+                class="tw-font-heading tw-text-base md:tw-text-lg xl:tw-text-2xl"
+              >
+                {{ $t('5YearTitle') }}
+              </h1>
+              <small class="tw-text-sm md:tw-text-base xl:tw-text-lg">
+                {{ $t('5YearSubtitle') }}
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <canvas
+        ref="confetti"
+        class="tw-pointer-events-none tw-absolute tw-inset-0 tw-h-full tw-w-full"
+      />
     </div>
     <div class="tw-container tw-mx-auto tw-p-4">
       <div class="tw-flex tw-items-center tw-gap-x-2">
@@ -232,6 +297,7 @@ import {
   mdiDownload,
   mdiCameraOutline,
 } from '@mdi/js'
+import confetti from 'canvas-confetti'
 
 export default {
   middleware: 'loadData',
@@ -352,6 +418,21 @@ export default {
       return this.$store.state.agencies.times
     },
   },
+  mounted() {
+    const canvas = this.$refs.confetti
+
+    canvas.confetti =
+      canvas.confetti ||
+      confetti.create(canvas, {
+        resize: true,
+        useWorker: true,
+        disableForReducedMotion: true,
+      })
+
+    setTimeout(() => {
+      this.launchConfetti()
+    }, 500)
+  },
   methods: {
     addAll() {
       Object.keys(this.availableAgencies)
@@ -362,6 +443,46 @@ export default {
             this.availableAgencies[slug]
           )
         })
+    },
+    launchConfetti() {
+      const canvas = this.$refs.confetti
+      const randomInRange = (min, max) => Math.random() * (max - min) + min
+      const colors = this.$vuetify.theme.dark
+        ? [
+            '#96CCFF',
+            '#003353',
+            '#004A75',
+            '#CEE5FF',
+            '#53DBCA',
+            '#003732',
+            '#005048',
+            '#74F8E6',
+          ]
+        : [
+            '#00639A',
+            '#CEE5FF',
+            '#001D32',
+            '#FFFFFF',
+            '#006A60',
+            '#74F8E6',
+            '#00201C',
+          ]
+
+      canvas.confetti({
+        angle: randomInRange(40, 80),
+        spread: 55,
+        particleCount: randomInRange(150, 200),
+        origin: { y: 1.5, x: 0 },
+        colors,
+      })
+
+      canvas.confetti({
+        angle: randomInRange(100, 140),
+        spread: 55,
+        particleCount: randomInRange(150, 200),
+        origin: { y: 1.5, x: 1 },
+        colors,
+      })
     },
     open(setting) {
       this.$store.commit('app/set', { key: 'open' + setting, value: true })
@@ -393,7 +514,9 @@ export default {
     "brandSlogan": "Making real time transit data accessible",
     "forDevelopers": "For Developers",
     "exoVin": "exo VIN Project",
-    "github": "On GitHub"
+    "github": "On GitHub",
+    "5YearTitle": "Happy Birthday Transit Tracker!",
+    "5YearSubtitle": "5 years of sightings, 14000+ vehicles, 40 agencies, 6 regions. Thank you for being part of this adventure!"
   },
   "fr": {
     "welcome": "Bienvenue sur",
@@ -416,7 +539,9 @@ export default {
     "brandSlogan": "Rendre accessibles les données de transport en temps réel",
     "forDevelopers": "Site pour les développeurs",
     "exoVin": "Projet exo VIN",
-    "github": "Sur GitHub"
+    "github": "Sur GitHub",
+    "5YearTitle": "Joyeux anniversaire Transit Tracker!",
+    "5YearSubtitle": "5 ans d'exploration, 14000+ véhicules, 40 agences, 6 régions. Merci de faire partie de cette aventure!"
   }
 }
 </i18n>
