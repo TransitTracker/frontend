951873df8f56e1a3171cd52444a189442383e0c0
Félix Desjardins <FelixINX@users.noreply.github.com>
2026-09-05 12:15:51 -0400

    💄 Small fix to VehicleSheet UI

--- components/VehicleSheet/VehicleSheet.vue
+++ components/VehicleSheet/VehicleSheet.vue
@@ -2,7 +2,7 @@
   <div class="tw:contents">
     <!-- Desktop sheet: floating on the left side of the screen -->
     <aside
-      class="tw:absolute tw:left-2 tw:top-2 tw:z-10 tw:hidden tw:max-h-[calc(100vh-41px-32px)] tw:w-96 tw:space-y-2 tw:overflow-y-auto tw:rounded-xl tw:bg-neutral-99 tw:p-4 tw:pb-4 tw:text-neutral-10 tw:shadow-none tw:dark:bg-neutral-10 tw:dark:text-neutral-90 tw:md:block tw:xl:left-4 tw:xl:top-4 tw:xl:space-y-4"
+      class="tw:absolute tw:z-10 tw:left-2 tw:top-2 tw:hidden tw:max-h-[calc(100vh-41px-32px)] tw:w-96 tw:space-y-2 tw:overflow-y-auto tw:rounded-xl tw:bg-neutral-99 tw:p-4 tw:pb-4 tw:text-neutral-10 tw:shadow-none tw:dark:bg-neutral-10 tw:dark:text-neutral-90 tw:md:block tw:xl:left-4 tw:xl:top-4 tw:xl:space-y-4"
     >
       <VehicleSheetHeader :vehicle="vehicle" />
       <VehicleSheetContent :vehicle="vehicle" />
@@ -11,17 +11,16 @@
     <!-- Mobile sheet: bottom sheet -->
     <bottom-sheet
       tabindex="0"
-      class="vehicle-sheet-bottom tw:text-neutral-10 tw:dark:text-neutral-90 tw:md:hidden"
+      class="vehicle-sheet-bottom tw:text-neutral-10 tw:dark:text-neutral-90 tw:z-10 tw:md:hidden"
     >
-      <div slot="snap" style="--snap: 75%"></div>
-      <div slot="snap" style="--snap: 40%"></div>
+      <div slot="snap" style="--snap: 50%"></div>
       <div
         slot="snap"
         style="--snap: calc(var(--tw-spacing) * 22)"
         class="initial"
       ></div>
 
-      <div slot="header" class="tw:relative tw:z-10 tw:px-4 tw:pb-2">
+      <div slot="header" class="tw:relative tw:px-4 tw:pb-2">
         <VehicleSheetHeader :vehicle="vehicle" />
       </div>
 
@@ -57,9 +56,8 @@ export default {
 bottom-sheet.vehicle-sheet-bottom {
   --sheet-background: var(--tw-color-neutral-96);
   --sheet-border-radius: 1.75rem;
-  --sheet-max-height: calc(100dvh - calc(var(--tw-spacing) * 32));
+  --sheet-max-height: calc(100dvh - calc(var(--tw-spacing) * 72));
   bottom: calc(var(--tw-spacing) * 16);
-  z-index: 20;
 }
 
 :where([data-theme='dark'], [data-theme='dark'] *)
@@ -69,10 +67,6 @@ bottom-sheet.vehicle-sheet-bottom {
 
 bottom-sheet.vehicle-sheet-bottom::part(sheet) {
   box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
-}
-
-bottom-sheet.vehicle-sheet-bottom::part(header) {
-  z-index: 10;
 }
 
 bottom-sheet.vehicle-sheet-bottom::part(handle) {
